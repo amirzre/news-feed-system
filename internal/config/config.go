02e35eaeacb26a7d185671b3cabf12e72e383bc7
Amir <amirzre@email.com>
2025-08-04 09:44:40 +0330

    feat: create application config models.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,57 @@
 package config
+
+import "time"
+
+type Config struct {
+	Database DatabaseConfig
+	Redis    RedisConfig
+	Server   ServerConfig
+	NewsAPI  NewsAPIConfig
+	App      AppConfig
+	Cache    CacheConfig
+	CORS     CORSConfig
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
+
+type RedisConfig struct {
+	Host     string
+	Port     int
+	Password string
+	DB       int
+}
+
+type ServerConfig struct {
+	Host string
+	Port int
+}
+
+type NewsAPIConfig struct {
+	APIKey  string
+	BaseURL string
+}
+
+type AppConfig struct {
+	Environment string
+	LogLevel    string
+}
+
+type CacheConfig struct {
+	TTL time.Duration
+}
+
+type CORSConfig struct {
+	AllowOrigins     []string
+	AllowMethods     []string
+	AllowHeaders     []string
+	ExposeHeaders    []string
+	AllowCredentials bool
+	MaxAge           int
+}
