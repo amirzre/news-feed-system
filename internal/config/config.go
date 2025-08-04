7f8abc753825bd48f5607311007a673d6737f04a
Amir <amirzre@email.com>
2025-08-04 11:55:06 +0330

    feat: add get time duration variable helper function.

--- internal/config/config.go
+++ internal/config/config.go
@@ -197,6 +197,20 @@ func getEnvBool(key string, fallback bool) bool {
 	return fallback
 }
 
+func getEnvDuration(key string, fallback time.Duration) time.Duration {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+
+	duration, err := time.ParseDuration(value)
+	if err != nil {
+		return fallback
+	}
+
+	return duration
+}
+
 func getEnvStringSlice(key string, fallback []string) []string {
 	value := os.Getenv(key)
 	if value == "" {
