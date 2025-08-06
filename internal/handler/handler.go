9f6946db0d62768df9662b0802b5076af7f8105f
Amir <amirzre@email.com>
2025-08-06 13:33:08 +0330

    fix: change handlers structure to support all entity handlers.

---
+++ internal/handler/handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"github.com/amirzre/news-feed-system/internal/service"
+	"github.com/amirzre/news-feed-system/pkg/logger"
+	"github.com/labstack/echo/v4"
+)
+
+// PostHandler defines the contract for post HTTP handlers
+type PostHandler interface {
+	CreatePost(c echo.Context) error
+}
+
+// Handler holds all handler implementations
+type Handler struct {
+	Post PostHandler
+}
+
+// New creates a new handler instance with all entity handlers
+func New(svc *service.Service, logger *logger.Logger) *Handler {
+	return &Handler{
+		Post: NewPostHandler(svc.Post, logger),
+	}
+}
